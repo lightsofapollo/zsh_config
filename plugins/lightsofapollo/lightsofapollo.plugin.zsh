b212db1f955e517f15a4f89fab43783993338eef
James Lal <james@lightsofapollo.com>
2014-06-26 11:08:01 -0700

    v1

--- plugins/lightsofapollo/lightsofapollo.plugin.zsh
+++ plugins/lightsofapollo/lightsofapollo.plugin.zsh
@@ -4,6 +4,7 @@ alias gau="git add -u"
 alias gc="git commit"
 alias gp="git push"
 alias gd="git diff"
+alias gaa="git add --all"
 
 # Paths
 export PATH="/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin"
